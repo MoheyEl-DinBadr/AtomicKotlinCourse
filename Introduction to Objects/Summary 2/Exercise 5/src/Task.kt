482f510d2bfe80e83b74c6846b4de15022d9dfba
MoheyElDin Badr <MoheyElDin.Badr@gmail.com>
2023-04-08 03:49:57 +0200

    commit

--- Introduction to Objects/Summary 2/Exercise 5/src/Task.kt
+++ Introduction to Objects/Summary 2/Exercise 5/src/Task.kt
@@ -1,15 +1,19 @@
 // Summary2/Task5.kt
 package summaryIIExercise5
 
-fun va1() {}
+fun va1(vararg s:Int) {
+    println("va1:")
+    s.forEach { println(it) }
+}
 
-fun va2() {}
+fun va2(vararg s:Int) {
+    println("va2:")
+    va1(*s)
+}
 
 fun main() {
-/*
   va1(1, 2, 3)
   va2(4, 5, 6)
-*/
 }
 /* Expected output:
 va1:
